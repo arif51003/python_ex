651d19582f3ff53fdbc692b7647ed6cc84a0288b
Arif <arif51003@gmail.com>
2025-09-20 17:24:51 +0500

    Fayl ozgardi

--- # 1.py
+++ # 1.py
@@ -1,6 +1,161 @@
-# 1
+# I.1
 print('Hello World')
 
 
-# 2
+# I.2
+
+a=input("Ismingizni kiriting:")
+print(f'Salom {a}!')
+
+#I.3
+
+a=input('Ismingiz:')
+b=input('Familiyangiz:')
+print(a,b)
+
+#I.4
+
+a,b,c=1,2,3
+print(a,b,c,sep=',')
+
+#I.5
+
+print('python','is',end='\nfun')
+
+#I.6
+
+a=int(input("Yoshingz:"))
+print(f"siz {a} yoshingizdasiz va {2025-a}-yilda tug'ulgansiz")
+
+#I.7
+
+a='Welcome'
+print('*'*7,"Welcome",'*'*6)
+
+#I.8
+
+a=input("so'z kiriting:")
+b=int(input("son kiriting:"))
+print(f"{a}\n"*b)
+
+#I.9
+
+a=7
+print(f'Haftada {a} kun bor')
+
+#I.10
+
+a='Arif'
+b='Asror'
+c='Nemat'
+print(a,b,c,sep='\n')
+
+
+
+# II.1
+
+my_number=10
+print(my_number)
+
+# II.2
+
+my_decimal=3.14
+print(type(my_decimal))
+
+# II.3
+
+is_true=True
+print(is_true)
+
+# II.4
+
+name='Arif'
+age=22
+print(f'{name} {age} yoshida')
+
+# II.5
+
+x,y,z=3,5,2
+
+#II.6
+
+a=4
+b=a
+print(b)
+
+# II.7
+
+a='123'
+b=int(a)
+print(b)
+
+# II.8
+
+son=input('son kirit:')
+print(int(son))
+
+# II.9
+
+pi=3.14159
+print(f'pi qiymati [{pi}]')
+
+# II.10
+
+a=4
+b=3
+a,b = b,a
+print(a,b)
+
+
+
+# III.1
+
+num1, num2 = 15, 4
+print("yig'indi=",num1+num2)
+
+# III.2
+
+print("50 va 23 orasidagi ayirma=",50-23)
+
+# III.3
+
+a=5*9
+print("ko'paytma=",a)
+
+# III.4
+
+a=100/10
+print('100/10=',int(a))
+
+# III.5
+
+a=17//3
+print('17//3=',a)
+
+# III.6
+
+a=25 % 7
+print(f'25 bolingsn 7dan qoldiq:{a}')
+
+# III.7
+
+a,b,c=10,5,2
+print((a+b)*c)
+
+# III.8
+
+a=int(input('son kirit:'))
+b=int(input('son kirit:'))
+print(f"yig'indi:{a+b}, ayirma:{a-b}, ko'paytma:{a*b}, bo'linma:{a/b}")
+
+# III.9
+
+a=int(input('Qiymqt='))
+print(f"Kvadrati:{a*a}")
+
+# III.10
+
+futbolka=25
+shapka=15
+print(f"umumiy hisobda {futbolka+shapka}$ sarfladi \n Yani {(futbolka+shapka)*12350} so'm sarflagan")
 
